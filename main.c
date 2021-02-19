cd4875f752c0a47c2dc305879098ea1f2e4fac38
Steven Quan <squan@buffalo.edu>
2021-02-18 19:53:00 -0500

    basic stuff

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc == 3){
+        //open file and count fields or records
+    }
+    else if (argc == 5){
+        //open file and check min/max/mean
+    }
+    else {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int min(){
+    
+}
+
+int max(){
+
+}
+
+int mean(){
+    
+}
